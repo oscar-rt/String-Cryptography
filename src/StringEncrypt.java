f505ad4ffbff2322d52f7aadbdeaf0f2332f1ce9
Oscar <oscarrtech@gmail.com>
2018-08-19 16:53:18 -0400

    Class Layout

---
+++ src/StringEncrypt.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Oscar
+ */
+public class StringEncrypt {
+    
+    public StringEncrypt(String text, String passkey){
+        
+        
+    }
+    
+}
