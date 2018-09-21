32f16b670cc8d6c50f69d3e4d2e846c9dbd8c87e
oscar-rt <oscarrtech@gmail.com>
2018-09-20 21:59:21 -0400

    Beta 1.01.2 Stable -BUGFIX: Now adds courses with multiple sections.

--- src/Cypher.java
+++ src/Cypher.java
@@ -1,4 +1,7 @@
 
+import java.util.ArrayList;
+
+
 /**
  *
  * @author Oscar
@@ -12,9 +15,20 @@ public class Cypher {
     private char operation; //'E' = encrypt, 'D' = decrypt
     private String passkey;
     
+    private String openingBracket;
+    private String closingBracket; //Brackets essentially referring to the surrounding hashes
+    private final int HASH_STRING_SIZE = 43;
+    
+    private final char alphanum[] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
+                                    'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+                                    '0','1','2','3','4','5','6','7','8','9', ' ', '.'};
+    private ArrayList<Character> cypher;
+    
     public Cypher(char operation, String passkey){
         this.operation = operation;
         this.passkey = passkey;
     }
+    
+    
     
 }
